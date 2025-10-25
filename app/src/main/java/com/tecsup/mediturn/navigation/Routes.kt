f39cafc14762f9fb2966cb2383bcdbffb8505e70
milennne <milene.fuentes@tecsup.edu.pe>
2025-10-25 01:07:13 -0500

    Agrego componente BottomBar y actualizo navegación y pantallas

--- app/src/main/java/com/tecsup/mediturn/navigation/Routes.kt
+++ app/src/main/java/com/tecsup/mediturn/navigation/Routes.kt
@@ -12,4 +12,6 @@ sealed class Routes (val route: String){
     object Login : Routes("login")
     object Register: Routes("register")
     object Home : Routes("home")
+    object Citas : Routes("citas")
+    object Perfil : Routes("perfil")
 }
