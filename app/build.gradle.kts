36bb66051a51f3396290e74001bb81dde6a6c6b1
Bryan <bryan@MacBook-Air-BC.local>
2025-10-25 22:42:52 -0500

    chore: agregar dependencia de DataStore en build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ android {
 
 dependencies {
 
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
     implementation("androidx.navigation:navigation-compose:2.9.5")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
